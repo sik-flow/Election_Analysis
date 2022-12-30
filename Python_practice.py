ddbaed13571b02d15e6af218943d2b54698668e3
Jeff Herman <jherman1199@gmail.com>
2022-12-30 11:57:12 -0600

    initial push

---
+++ Python_practice.py
@@ -0,0 +1,5 @@
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
